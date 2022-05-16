e3bffec2fe0d55a8f2482fb384e40695621b0f58
u/sayuri <54628820+sayuriu@users.noreply.github.com>
2022-05-16 23:21:00 +0700

    160522

--- components/body.tsx
+++ components/body.tsx
@@ -2,10 +2,12 @@ import Image from 'next/image';
 import { Box } from "@chakra-ui/react";
 import { useAtom } from "jotai";
 import { Language } from "@states/global";
-import { FC, useEffect } from "react";
-import { AnimatePresence, motion } from "framer-motion";
+import { FC, useEffect, useState } from "react";
+import { AnimatePresence, motion, useAnimation } from "framer-motion";
 
 import bodyStyles from './body-styles/Body.module.scss';
+import { AnimFunctions } from "@utils/anims";
+import Forceful = AnimFunctions.Forceful;
 
 export const Body = () => {
     const [currentLanguage] = useAtom(Language);
@@ -19,7 +21,7 @@ export const Body = () => {
         className={"rel"}
     >
         <AnimatePresence>
-            <Index index={3}/>
+            <Index key={"indexPad"}/>
         </AnimatePresence>
     </Box>);
 };
@@ -28,19 +30,75 @@ interface IIndex {
     index: number;
 }
 
-const Index: FC<IIndex> = ({ index }) => {
+const Index: FC = () => {
+    const [indexSubL, setIndexSubL] = useState(0);
+    const [indexSubU, setIndexSubU] = useState(0);
+    const [indexMain, setIndexMain] = useState(0);
+
+    const indexMainController = useAnimation();
+    const indexSubLController = useAnimation();
+    const indexSubUController = useAnimation();
+
+    const [isDecrementing, setIsDecrementing] = useState(false);
+    useEffect(() => {
+
+    }, []);
+
+    const transition = {
+        duration: 0.5,
+        ease: Forceful,
+    };
+    const indexNumConfig = {
+        className: "no-pointer",
+        stroke: "#1D1D1D",
+        fill: "#1D1D1D",
+        fontFamily: "Jetbrains Mono",
+        fontStyle: "italic",
+        fontSize: 1000,
+    };
+
+    const setIndexTo = (index: number) => {
+        setIndexMain(index);
+    };
+    const HandleScroll = (event: WheelEvent) => {
+        const up = event.deltaY < 0;
+        setIsDecrementing(up);
+        if ((indexMain < 2 && up) || (indexMain > 8 && !up)) return;
+        setIndexMain(indexMain + (up ? -1 : 1));
+    };
     return (
         <motion.svg className={"fh"} layout viewBox="0 29 496 906" fill="none" xmlns="http://www.w3.org/2000/svg">
             <g id="Rectangle 1" filter="url(#filter0_bd_140_3)">
                 <path d="M-1 29H495L233.935 935.347H-1V29Z" fill="white" fillOpacity="0.47" />
             </g>
-            <g id="Rectangle 3" filter="url(#filter1_d_140_3)">
-                <path d="M0 29H419.424L162.007 935.694L0 935.694V29Z" fill="black" />
-            </g>
+                <g clipPath={"url(#index__inner-poly)"}>
+                    <path id="Rectangle 3" onWheel={(e) => HandleScroll(e as unknown as WheelEvent)} filter="url(#filter1_d_140_3)" d="M0 29H419.424L162.007 935.694L0 935.694V29Z" fill="black" />
+                    <motion.text
+                        {...indexNumConfig}
+                        animate={indexSubUController}
+                    >
+                        {indexSubU}
+                    </motion.text>
+                    <motion.text
+                        {...indexNumConfig}
+                        initial={{ y: -100 }}
+                        animate={{ y: 0 }}
+                        transition={{
+                            duration: 0.5,
+                            ease: [0.88, -0.07, 0.22, 1.01]
+                        }}
+                        exit={{ y: -100 }} x={-180} y={830}
 
-            <g clipPath={"url(#index__inner-poly)"}>
-                <text x={-180} y={830} stroke={"#1D1D1D"} fill={"#1D1D1D"} fontFamily={"Jetbrains Mono"} fontStyle={"italic"} fontSize={1000} >{index}</text>
-            </g>
+                    >
+                        {indexMain}
+                    </motion.text>
+                    <motion.text
+                        {...indexNumConfig}
+                        animate={indexSubLController}
+                    >
+                        {indexSubL}
+                    </motion.text>
+                </g>
             <defs>
                 <clipPath id={"index__inner-poly"}>
                     <path d="M0 29H419.424L162.007 935.694L0 935.694V29Z"/>
