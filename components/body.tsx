76cb9ca50708834822e4e2cc94ea1c1f92d049ae
sayuri <54628820+sayuriu@users.noreply.github.com>
2022-05-17 14:21:33 +0700

    170522

--- components/body.tsx
+++ components/body.tsx
@@ -18,7 +18,8 @@ export const Body = () => {
         h="calc(100vh - 176px)"
         w="100vw"
         p={0}
-        className={"rel"}
+        className={"rel grid"}
+        bg={"#fff"}
     >
         <AnimatePresence>
             <Index key={"indexPad"}/>
@@ -31,17 +32,24 @@ interface IIndex {
 }
 
 const Index: FC = () => {
-    const [indexSubL, setIndexSubL] = useState(0);
     const [indexSubU, setIndexSubU] = useState(0);
-    const [indexMain, setIndexMain] = useState(0);
+    const [indexMain, setIndexMain] = useState(1);
+    const [indexSubL, setIndexSubL] = useState(2);
+    const [isAnimating, setIsAnimating] = useState(false);
 
     const indexMainController = useAnimation();
     const indexSubLController = useAnimation();
     const indexSubUController = useAnimation();
 
-    const [isDecrementing, setIsDecrementing] = useState(false);
     useEffect(() => {
-
+        indexMainController.set({ y: 830 });
+        indexSubLController.set({ y: 1670 });
+        indexSubUController.set({ y: -10 });
+        return () => Promise.all([
+            indexMainController.stop(),
+            indexSubLController.stop(),
+            indexSubUController.stop()
+        ]);
     }, []);
 
     const transition = {
@@ -55,19 +63,48 @@ const Index: FC = () => {
         fontFamily: "Jetbrains Mono",
         fontStyle: "italic",
         fontSize: 1000,
+        x: -180,
     };
 
-    const setIndexTo = (index: number) => {
-        setIndexMain(index);
-    };
     const HandleScroll = (event: WheelEvent) => {
-        const up = event.deltaY < 0;
-        setIsDecrementing(up);
-        if ((indexMain < 2 && up) || (indexMain > 8 && !up)) return;
-        setIndexMain(indexMain + (up ? -1 : 1));
+        if (isAnimating) return;
+        const down = event.deltaY < 0;
+        //               LOWER_INDEX_BOUND           UPPER_INDEX_BOUND
+        if ((indexMain < 2 && !down) || (indexMain > 2 && down)) return;
+        shiftNumberSequence(down);
+    };
+    const shiftNumberSequence = async (increment: boolean) => {
+        setIsAnimating(true);
+        if (increment) {
+            await Promise.all([
+                indexSubLController.start({ y: 830 }),
+                indexMainController.start({ y: -10 }),
+            ]);
+            setIndexMain(indexMain + 1);
+            await Promise.all([
+                indexMainController.set({ y: 830 }),
+                indexSubLController.set({ y: 1670 }),
+            ])
+            setIndexSubU(indexSubU + 1);
+            setIndexSubL(indexSubL + 1);
+        }
+        else {
+            await Promise.all([
+                indexSubUController.start({ y: 830 }),
+                indexMainController.start({ y: 1670 })
+            ]);
+            setIndexMain(indexMain - 1);
+            await Promise.all([
+                indexMainController.set({ y: 830 }),
+                indexSubUController.set({ y: -10 })
+            ]);
+            setIndexSubL(indexSubL - 1);
+            setIndexSubU(indexSubU - 1);
+        }
+        setIsAnimating(false);
     };
     return (
-        <motion.svg className={"fh"} layout viewBox="0 29 496 906" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <motion.svg className={"fh"} layout viewBox="0 29 520 880" fill="none" xmlns="http://www.w3.org/2000/svg">
             <g id="Rectangle 1" filter="url(#filter0_bd_140_3)">
                 <path d="M-1 29H495L233.935 935.347H-1V29Z" fill="white" fillOpacity="0.47" />
             </g>
@@ -75,29 +112,31 @@ const Index: FC = () => {
                     <path id="Rectangle 3" onWheel={(e) => HandleScroll(e as unknown as WheelEvent)} filter="url(#filter1_d_140_3)" d="M0 29H419.424L162.007 935.694L0 935.694V29Z" fill="black" />
                     <motion.text
                         {...indexNumConfig}
+                        initial={{ y: -10 }}
                         animate={indexSubUController}
+                        transition={transition}
                     >
                         {indexSubU}
                     </motion.text>
                     <motion.text
                         {...indexNumConfig}
-                        initial={{ y: -100 }}
-                        animate={{ y: 0 }}
-                        transition={{
-                            duration: 0.5,
-                            ease: [0.88, -0.07, 0.22, 1.01]
-                        }}
-                        exit={{ y: -100 }} x={-180} y={830}
-
+                        animate={indexMainController}
+                        transition={transition}
                     >
                         {indexMain}
                     </motion.text>
                     <motion.text
                         {...indexNumConfig}
                         animate={indexSubLController}
+                        transition={transition}
                     >
                         {indexSubL}
                     </motion.text>
+                    <g id={"label"}>
+                        <text>OVERVIEW</text>
+                        <text>PROTOCOL FIELD RECOVERYY DEPARTMENT</text>
+                        <text>COORDINATE RECORDS</text>
+                    </g>
                 </g>
             <defs>
                 <clipPath id={"index__inner-poly"}>
