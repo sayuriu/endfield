687d1e011cb2b2b29cc20a1692fc110fb7bf3c7a
u/sayuri <54628820+sayuriu@users.noreply.github.com>
2022-08-10 19:17:17 +0700

    100822: refactor: i forgot language

--- states/global.ts
+++ states/global.ts
@@ -1,11 +1,12 @@
 import { atom } from "jotai";
+import { localSet } from "@utils/common";
 
 const LanguageAtom = atom<string>("en");
 export const Language = atom<string, string, void>(
     (get) => get(LanguageAtom),
     (get, set, newLang) => {
-        if (window.localStorage) {
-            window.localStorage.setItem("preferredLanguage", newLang);
+        if ("localStorage" in window) {
+            localSet("preferredLanguage", newLang);
         }
         set(LanguageAtom, newLang);
     },
